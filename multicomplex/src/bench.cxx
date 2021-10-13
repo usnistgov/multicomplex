b878f2e7a1e1a474008be7a7f241a9607eb02785
Ian Bell <ian.bell@nist.gov>
2021-10-13 09:57:43 -0400

    Clean up and simplify the benchmarking script

--- multicomplex/src/bench.cxx
+++ multicomplex/src/bench.cxx
@@ -5,6 +5,8 @@
 
 #include "MultiComplex/MultiComplex.hpp"
 
+using mcxf = std::function<mcx::MultiComplex<double>(const mcx::MultiComplex<double>&)>;
+
 TEST_CASE("Time some bit things", "[bits]")
 {
 	BENCHMARK("exp2i") {
@@ -18,94 +20,162 @@ TEST_CASE("Time some bit things", "[bits]")
 TEST_CASE("time x^3", "[benchmark]")
 {
 	double x = 8.1;
-	auto f = [](double x) { return x*x*x; };
-	typedef std::function<mcx::MultiComplex<double>(const mcx::MultiComplex<double>&)> fcn_t;
-	fcn_t ff = [](const mcx::MultiComplex<double>& x) {
-		return x*x*x;
-	};
-	auto xmcx = mcx::MultiComplex<double>(x);
-	auto xcx = std::complex<double>(x,x);
+	auto f = [](const auto& x) { return x*x*x; };
 
 	BENCHMARK("function") {
 		return f(x);
 	};
 	BENCHMARK("call with mcx"){
-		return ff(xmcx);
+		auto xmcx = mcx::MultiComplex<double>(x);
+		return static_cast<mcxf>(f)(xmcx);
 	};
 	BENCHMARK("first deriv"){
-		return mcx::diff_mcx1(ff, x, 1, false);
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
 	};
 }
 
-TEST_CASE("time cos*sin", "[benchmark]")
+TEST_CASE("time x^3, but with pow(x,int)", "[benchmark]")
 {
 	double x = 8.1;
-	auto f = [](double x) { return cos(x) * sin(x); };
-	typedef std::function<mcx::MultiComplex<double>(const mcx::MultiComplex<double>&)> fcn_t;
-	fcn_t ff = [](const mcx::MultiComplex<double>& x) {
-		return cos(x) * sin(x);
-	};
-	auto xmcx = mcx::MultiComplex<double>(x);
+	auto f = [](const auto& x) { return pow(x, 3); };
 
-	BENCHMARK("function"){ 		
+	BENCHMARK("function") {
 		return f(x);
 	};
-	BENCHMARK("call with mcx"){
-		return ff(xmcx);
+	BENCHMARK("call with mcx") {
+		auto xmcx = mcx::MultiComplex<double>(x);
+		return static_cast<mcxf>(f)(xmcx);
 	};
-	BENCHMARK("first deriv"){	
-		return mcx::diff_mcx1(ff, x, 1, false);
+	BENCHMARK("first deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
 	};
 }
 
-TEST_CASE("time sin", "[benchmark],[sin]")
+TEST_CASE("time x^3, but with pow(x,double)", "[benchmark]")
 {
 	double x = 8.1;
-	auto f = [](double x) { return sin(x); };
-	typedef std::function<mcx::MultiComplex<double>(const mcx::MultiComplex<double>&)> fcn_t;
-	fcn_t ff = [](const mcx::MultiComplex<double>& x) {
-		return sin(x);
+	auto f = [](const auto& x) { return pow(x, 3.0); };
+
+	BENCHMARK("function") {
+		return f(x);
 	};
-	auto fgen = [](const std::complex<double>& x) {
-		return sin(x);
+	BENCHMARK("call with mcx") {
+		auto xmcx = mcx::MultiComplex<double>(x);
+		return static_cast<mcxf>(f)(xmcx);
 	};
-	auto xcx = std::complex<double>(x, 1e-100);
+	BENCHMARK("first deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
+	};
+}
+
+TEST_CASE("time 1/x", "[benchmark]")
+{
+	double x = 8.1;
+	auto f = [](const auto& x) { return 1.0/x; };
+
+	BENCHMARK("function") {
+		return f(x);
+	};
+	BENCHMARK("call with mcx") {
+		auto xmcx = mcx::MultiComplex<double>(x);
+		return static_cast<mcxf>(f)(xmcx);
+	};
+	BENCHMARK("first deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
+	};
+}
+
+TEST_CASE("time sin(x)", "[benchmark],[sin]")
+{
+	double x = 8.1;
+	auto f = [](const auto& x) { return sin(x); };
 
 	BENCHMARK("function") {
 		return f(x);
 	};
 	BENCHMARK("first deriv"){	
-		return mcx::diff_mcx1(ff, x, 1, false);
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
 	};
-	BENCHMARK("increment") {
-		return mcx::increment(4);
+}
+
+TEST_CASE("time cosh(x)", "[benchmark],[cosh]")
+{
+	double x = 8.1;
+	auto f = [](const auto& x) { return cosh(x); };
+
+	BENCHMARK("function") {
+		return f(x);
 	};
-	BENCHMARK("allocate 2 element valarray<double>") {
-		return std::valarray<double>(0.0, 2);
+	BENCHMARK("first deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
 	};
-	BENCHMARK("get the complex") {
-		auto xcx = mcx::MultiComplex<double>(x);
-		return xcx.complex();
-	}; 
-	BENCHMARK("call w/ MultiComplex<double>") {
-		auto xcx = mcx::MultiComplex<double>(x);
-		return ff(xcx);
+}
+
+TEST_CASE("time exp(x)", "[benchmark]")
+{
+	double x = 8.1;
+	auto f = [](const auto &x) { return exp(x); };
+	BENCHMARK("function") {
+		return f(x);
+	};
+	BENCHMARK("first deriv"){
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
 	};
 }
 
 TEST_CASE("time ln(x)", "[benchmark]")
 {
 	double x = 8.1;
-	auto f = [](double x) { return log(x); };
+	auto f = [](const auto& x) { return log(x); };
 	BENCHMARK("function") {
 		return f(x);
 	};
-	BENCHMARK("first deriv")
-	{
-		typedef std::function<mcx::MultiComplex<double>(const mcx::MultiComplex<double>&)> fcn_t;
-		fcn_t ff = [](const mcx::MultiComplex<double>& x) {
-			return log(x);
-		};
-		return mcx::diff_mcx1(ff, x, 1, false);
+	BENCHMARK("first deriv"){
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
+	};
+}
+
+TEST_CASE("time cos(x)*sin(x)", "[benchmark]")
+{
+	double x = 8.1;
+	auto f = [](const auto& x) { return cos(x) * sin(x); };
+	BENCHMARK("function") {
+		return f(x);
+	};
+	BENCHMARK("first deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
+	};
+}
+
+TEST_CASE("time alphar", "[benchmark]")
+{
+	double x = 8.1;
+	
+	// Critical point values for argon
+	double R = 8.31446261815324,
+		   Tc = 150.687, // K
+		   pc = 4863000.0, // Pa
+		   a = (27.0/64.0)*pow(R*Tc, 2)/pc, 
+		   b = (1.0/8.0)*(R*Tc)/pc,
+		   T = 298.15;
+	auto f = [&](const auto& rho) { return -log(1.0-b*rho)-a*rho/(R*T); };
+
+	BENCHMARK("function") {
+		return f(x);
+	};
+	BENCHMARK("first deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 1, false);
+	};
+	BENCHMARK("second deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 2, false);
+	};
+	BENCHMARK("third deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 3, false);
+	};
+	BENCHMARK("fourth deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 4, false);
+	};
+	BENCHMARK("fifth deriv") {
+		return mcx::diff_mcx1(static_cast<mcxf>(f), x, 5, false);
 	};
 }
